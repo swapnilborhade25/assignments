4b585837af6c42b80ff357df6e9edd7ad157351c
swapnilborhade25 <swapnilborhade25@gmail.com>
2026-08-31 20:44:19 +0530

    assignment 2 completed

---
+++ assign02_empl.js
@@ -0,0 +1,46 @@
+
+// creating employees object array
+const employees =[
+    {
+        id:1,
+        name : "Swapnil",
+        department : "IT",
+        salary : 60000
+    },
+    {
+        id :2,
+        name : "Rahul",
+        department : "Finance",
+        salary : 50000
+    },
+    {
+        id : 3,
+        name : "Rutuja",
+        department : "HR",
+        salary : 40000
+    },
+    {
+        id : 4,
+        name : "Sam",
+        department : "IT",
+        salary : 45000
+    }
+]
+// console.log(employees);
+
+console.log("filter by department")
+//filter by department
+const itEmployee = employees.filter( employee => {
+    return employee.department === "IT";
+});
+console.log(itEmployee);
+
+
+// sorted by salary
+console.log("Descending sorting ")
+const sortBySalary = [...employees].sort((a,b) =>{
+    return b.salary - a.salary;
+});
+
+console.log(sortBySalary);
+
